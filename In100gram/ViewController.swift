0303ee31d3bfcdda7d0a125297b6d6650012f70d
Артем Полушин <tomas@macbook-pro-artem.local>
2018-08-19 14:08:14 +0300

    Initial Commit

---
+++ In100gram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  In100gram
+//
+//  Created by Артем Полушин on 19.08.2018.
+//  Copyright © 2018 Артем Полушин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
